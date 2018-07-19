2363fdc2c2ea48e8bbdfc0ae0726ef14ded5816e
Marcus Turnbo <marcus@emarcus.com>
2018-07-19 17:12:49 -0500

    tile selected change color

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,13 +1,17 @@
 # Grid Traversal Demo
 
+#### v.0.1.5
+* *Feature*: Tile onClick change color behavior
+
 #### v.0.1.4
-* *Feature*: Added Header and Footer components 
+* *Feature*: Header and Footer components
+* *Feature*: favicon
 
 #### v.0.1.3
-* *Feature*: Added Grid container
+* *Feature*: Grid container
 
 #### v.0.1.2
-* *Feature*: Added Tile component
+* *Feature*: Tile component
 
 #### v.0.1.1
 * *Feature*: Basic React Grid with Webpack
--- src/components/Tile/Tile.jsx
+++ src/components/Tile/Tile.jsx
@@ -2,13 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Tile.scss';
 
-const Tile = ({row, col}) => {
-  return (
-    <div className="tile">
-      {row}:{col}
-    </div>
-  );
-};
+class Tile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {active: false};
+  }
+
+  toggleActive() {
+    this.setState({active: !this.state.active});
+  }
+
+  render() {
+    return (
+      <div className={'tile' + (this.state.active ? ' tile-selected' : '')} onClick={this.toggleActive.bind(this)}>
+        {this.props.row}:{this.props.col}
+      </div>
+    );
+  }
+}
 
 Tile.propTypes = {
   row: PropTypes.string.isRequired,
