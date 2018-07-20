12dfb694a2c088f4ac46a5d8d87f40780a0c07ac
Marcus Turnbo <marcus@emarcus.com>
2018-07-20 15:41:43 -0500

    Added combo calculation function

--- src/components/Formula/Formula.jsx
+++ src/components/Formula/Formula.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import MathJax from 'react-mathjax';
+import Combo from '../../combos.js';
 
 const Formula = (props) => {
   const formula = 'C_k(n) = {n! \\over k!(n - k)!}';
-  const solution = `C_${props.k}(${props.n}) = {${props.n}! \\over ${props.k}!(${props.n} - ${props.k})!} = ${props.s}`;
+  const answer = Combo([1,1],[5,3]);
+  const solution = `C_${props.k}(${props.n}) = {${props.n}! \\over ${props.k}!(${props.n} - ${props.k})!} = ${answer}`;
 
   return (
     <MathJax.Provider>
